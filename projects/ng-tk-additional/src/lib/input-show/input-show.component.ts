3a6532fe6b314f1c8f66c83921d8cc57894452c2
tinyking <akingchao@qq.com>
2020-01-22 14:48:28 +0800

    1

--- projects/ng-tk-additional/src/lib/input-show/input-show.component.ts
+++ projects/ng-tk-additional/src/lib/input-show/input-show.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tk-input-show',
   templateUrl: './input-show.component.html',
-  styleUrls: ['./input-show.component.css']
+  styleUrls: ['./input-show.component.scss']
 })
-export class InputShowComponent implements OnInit {
-
-  constructor() { }
+export class InputShowComponent implements OnInit, OnDestroy {
+  /**
+   * Message to display in the InputShow
+   */
+  message: string;
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+  }
 }
--- projects/ng-tk-additional/src/lib/input-show/input-show.module.ts
+++ projects/ng-tk-additional/src/lib/input-show/input-show.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputShowDirective } from './input-show.directive';
+import { InputShowComponent } from './input-show.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InputShowDirective, InputShowComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [InputShowDirective]
 })
 export class InputShowModule { }
