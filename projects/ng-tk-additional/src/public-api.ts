9a183b3a7804be53765fc4d232b83dbd1c3e8537
tinyking <akingchao@qq.com>
2020-01-22 15:00:54 +0800

    remove old library

--- projects/ng-tk-additional/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-tk-additional
- */
-
-export * from './lib/ng-tk-additional.service';
-export * from './lib/ng-tk-additional.component';
-export * from './lib/ng-tk-additional.module';
